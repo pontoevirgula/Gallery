27d98852c1ece94c697147c4d52f318b8a3539e8
carloslima <T3549833@timbrasil.com.br>
2023-03-08 12:20:58 -0300

    config hilt

---
+++ app/src/main/java/com/chslcompany/gallery/framework/di/NetworkModule.kt
@@ -0,0 +1,72 @@
+package com.chslcompany.gallery.framework.di
+
+import com.chslcompany.gallery.BuildConfig
+import com.chslcompany.gallery.framework.network.AuthorizationInterceptor
+import com.chslcompany.gallery.framework.network.api.WallpapersApi
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val TIME_OUT_SECONDS = 15L
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        setLevel(
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        )
+    }
+
+    @Provides
+    fun provideAuthorizationInterceptor(): AuthorizationInterceptor =
+        AuthorizationInterceptor(BuildConfig.PRIVATE_KEY)
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
+    ) : OkHttpClient =
+        OkHttpClient.Builder()
+            .readTimeout(TIME_OUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIME_OUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(TIME_OUT_SECONDS, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authorizationInterceptor)
+            .build()
+
+
+    @Provides
+    fun provideGson() : Gson =
+        GsonBuilder()
+            .setLenient()
+            .create()
+
+    @Provides
+    fun provideGsonConverterFactory(gson: Gson) : GsonConverterFactory =
+        GsonConverterFactory.create(gson)
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, gsonConverterFactory: GsonConverterFactory) : WallpapersApi =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .build()
+            .create(WallpapersApi::class.java)
+
+}
