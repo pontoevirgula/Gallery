de015412bf2b0580bcf96256a258f5cb49da8582
carloslima <T3549833@timbrasil.com.br>
2023-03-20 20:33:57 -0300

    criado tesst usecase

--- app/src/test/java/com/chslcompany/gallery/ui/fragment/popular/viewmodel/PopularViewModelTest.kt
+++ app/src/test/java/com/chslcompany/gallery/ui/fragment/popular/viewmodel/PopularViewModelTest.kt
@@ -1,10 +1,9 @@
 package com.chslcompany.gallery.ui.fragment.popular.viewmodel
 
 import androidx.paging.PagingData
-import com.chslcompany.core.model.PhotoDomain
-import com.chslcompany.core.model.SrcDomain
 import com.chslcompany.core.usecase.popularusecase.GetPopularUseCase
 import com.chslcompany.testing.MainCoroutineRule
+import com.chslcompany.testing.model.WallpapersFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
@@ -38,7 +37,7 @@ class PopularViewModelTest {
 
     @Test
     fun `Should validate pagination data`() = runTest {
-        whenever(popularUseCase(any())).thenReturn(flowOf(getPagingDataMock()))
+        whenever(popularUseCase(any())).thenReturn(flowOf(getPagingDataMock))
         val result = viewModel.popularWallPapers()
         assertNotNull(result.first())
     }
@@ -51,46 +50,16 @@ class PopularViewModelTest {
         viewModel.popularWallPapers()
     }
 
+    private val wallpapersFactory = WallpapersFactory()
 
-    //not working pois nem sempre da pra testar o framework(paging)
-    //comentei pra passar na esteira
-//    @Test
-//    fun `Should validate pagination data2`() = runTest {
-//        val expected = getPagingDataMock()
-//        whenever(popularUseCase(any())).thenReturn(flowOf(getPagingDataMock()))
-//        val result = viewModel.popularWallPapers().first()
-//        result.map { resultPhoto ->
-//            expected.map {
-//                assertEquals(it, resultPhoto)
-//            }
-//
-//        }
-//    }
+    private val getPagingDataMock =
+        PagingData.from(
+            listOf(
+                wallpapersFactory.create(
+                    WallpapersFactory.Photo.PhotoDomainSuccess
+                )
+            )
+        )
 
-    private fun getPagingDataMock() =
-        PagingData.from(listOf(domain, domain, domain, domain, domain, domain))
-
-    private val domain = PhotoDomain(
-        description = "Free stock photo of agriculture, architecture, bright",
-        avgColor = "#56543B",
-        height = 6240,
-        id = 15575287,
-        liked = false,
-        photographer = "medinegurbet",
-        photographerId = 21435626,
-        photographerUrl = "https://www.pexels.com/@medinegurbet-21435626",
-        srcDomain = SrcDomain(
-            landscape = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-            large = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
-            large2x = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            medium = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&h=350",
-            original = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg",
-            portrait = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
-            small = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&h=130",
-            tiny = "https://images.pexels.com/photos/15575287/pexels-photo-15575287.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
-        ),
-        url = "https://www.pexels.com/photo/wood-summer-garden-house-15575287/",
-        width = 4160
-    )
 
 }
